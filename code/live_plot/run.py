32401f8097e0cad9a9dbca089f36d404110bfa7a
Felix Meyer zu Driehausen <vamo@uni-osnabrueck.de>
2019-08-13 21:47:27 +0200

    Set plotting to false

--- code/live_plot/run.py
+++ code/live_plot/run.py
@@ -1,18 +1,18 @@
+from ldtool.msg import Eog
+import rospy
+from utils import reref_channels
+from twisted.internet import reactor, defer, task
+from Traumschreiber import *
+import time
+import numpy as np
 import logging
 loglevel = logging.INFO
 logging.basicConfig(level=loglevel)
-import numpy as np
-import time
-from Traumschreiber import *
-from twisted.internet import reactor, defer, task
-from utils import reref_channels
-#ROS tools
-import sys
-sys.path.insert(0, "/home/felix/catkin_ws/src")
-import rospy
-from ldtool.msg import Eog
+# ROS tools
+#import sys
+#sys.path.insert(0, "/home/felix/catkin_ws/src")
 
-SHOWPLOT = True
+SHOWPLOT = False
 
 ########################################
 # ID of the traumschreiber you are using
@@ -22,42 +22,45 @@ GAIN = 4
 TRAUMSCHREIBER_ADDR = "74:72:61:75:6D:{:02x}".format(ID)
 # reference channel
 REF_CHANNEL = 4
-duration=25000
-data = np.zeros((duration,9), dtype='<i2')
- # Publishers
+duration = 25000
+data = np.zeros((duration, 9), dtype='<i2')
+# Publishers
 rospy.init_node("traumschreiber_data", anonymous=False)
-EOG_pub = rospy.Publisher("/EOG_data", Eog, queue_size=1)
-msg = Eog()
+EOG_pub = rospy.Publisher("/EOG_data", Eog, queue_size=0)
+
 
 def data_callback(data_in):
     global data
-    
+    print(data.shape)
     data = np.roll(data, -1, axis=0)
-    data[-1,:] = np.hstack(([[0]],data_in))
-    data[-1,:] = reref_channels(data_in, REF_CHANNEL)
+    #data[-1, :] = np.hstack(([[0]], data_in))
+    data[-1, :] = reref_channels(data_in, REF_CHANNEL)
     #new_row = [time.time(),data[-1,:][0],data[-1,:][1],data[-1,:][2]]
-    
+    msg = Eog()
     msg.timestamp = time.time()
-    msg.ch0 = data[-1,:][0]
-    msg.ch1 = data[-1,:][1]
-    msg.ch2 = data[-1,:][2]
+    msg.ch0 = data[-1, :][0]
+    msg.ch1 = data[-1, :][1]
+    msg.ch2 = data[-1, :][2]
     print(msg)
     EOG_pub.publish(msg)
+
 
 if SHOWPLOT:
     import matplotlib
     matplotlib.use("TkAgg")
     from matplotlib import pyplot as pp
+
     def plot():
         try:
-            for i,line in enumerate(lines):
+            for i, line in enumerate(lines):
                 fig.canvas.restore_region(background[i])
-                line.set_data(tt, data[:,i])
+                line.set_data(tt, data[:, i])
                 ax[i].draw_artist(line)
                 #fig.canvas.set_window_title("Data (received {} packages/second)".format(pkgs_per_second))
                 fig.canvas.blit(ax[i].bbox)
         except Exception as e:
             print("Encountered exception in plot callback: {}".format(e))
+
 
 async def run():
     async with Traumschreiber(addr=TRAUMSCHREIBER_ADDR) as t:
@@ -81,6 +84,7 @@ async def run():
         if SHOWPLOT:
             plot()
 
+
 def main(reactor):
     d = defer.ensureDeferred(run())
     return d
@@ -88,13 +92,15 @@ def main(reactor):
 
 if SHOWPLOT:
     # Plot lines
-    fig, ax = pp.subplots(nrows=9, ncols=1, figsize=(15,10), sharex=True, sharey=True)
+    fig, ax = pp.subplots(nrows=9, ncols=1, figsize=(
+        15, 10), sharex=True, sharey=True)
     fig.show()
     fig.canvas.draw()
 
     tt = np.arange(duration)
-    lines = [ax[i].plot(tt, data[:,i])[0] for i in range(9)]
-    hlines = [(ax[i].axhline(y=2**11, c="black"), ax[i].axhline(y=-2**11, c="black")) for i in range(9)]
+    lines = [ax[i].plot(tt, data[:, i])[0] for i in range(9)]
+    hlines = [(ax[i].axhline(y=2**11, c="black"),
+               ax[i].axhline(y=-2**11, c="black")) for i in range(9)]
     ax[0].set_ylim([-2**12, 2**12])
     background = [fig.canvas.copy_from_bbox(ax[i].bbox) for i in range(9)]
     task.LoopingCall(plot).start(0.5)
